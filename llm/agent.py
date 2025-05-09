865daf628a7b0aa5373f6216ff9370fb49d0d8a3
Asif Mohammad Mollah <admin@mrasif.in>
2025-05-09 10:30:05 +0400

    tools added

---
+++ llm/agent.py
@@ -0,0 +1,82 @@
+import os
+import json
+import time
+import requests
+
+def handle_tool_call(message):
+    tool_call = message.tool_calls[0]
+    if tool_call.function.name == "get_current_time":
+        time = get_current_time()
+        response = {
+            "role": "tool",
+            "content": json.dumps({"Time": time}),
+            "tool_call_id": tool_call.id
+        }
+        return response
+    elif tool_call.function.name == "get_weather":
+        arguments = json.loads(tool_call.function.arguments)
+        city = arguments.get('city')
+        weather = get_weather(city)
+        response = {
+            "role": "tool",
+            "content": json.dumps({"Weather": weather}),
+            "tool_call_id": tool_call.id
+        }
+        return response
+
+def getTools():
+	time_function = {
+	    "name": "get_current_time",
+	    "description": "Get the current time. Call this whenever you need to know the current time or date, for example when a user asked 'What is time right now? or 'What is today's date?'",
+	    "parameters": {
+	        "type": "object",
+	        "properties": {},
+	        "required": [],
+	        "additionalProperties": False
+	    }
+	}
+
+	weather_function = {
+	    "name": "get_weather",
+	    "description": "Get the weather details. Call this whenever you need to know the weather for a city, for example when a user asked 'How is weather now?', If you don't know the city name then ask user for city name.",
+	    "parameters": {
+	        "type": "object",
+	        "properties": {
+	            "city": {
+	                "type": "string",
+	                "description": "The name of the city you want to know about"
+	            }
+	        },
+	        "required": ["city"],
+	        "additionalProperties": False
+	    }
+	}
+
+	tools = [{"type": "function", "function": time_function}, {"type": "function", "function": weather_function}]
+	return tools
+
+def get_current_time():
+    print("Tool get_current_time called.")
+    os.environ['TZ'] = 'Asia/Dubai'
+    time.tzset()
+    return time.strftime("%Y-%m-%d %H:%M:%S")
+
+def get_weather(city):
+    print(f"Tool get_weather called for location {city}.")
+    base_url = "https://wttr.in/"+city
+    params = {
+        'format': 'j1',
+    }
+
+    try:
+        response = requests.get(base_url, params=params)
+        response.raise_for_status()
+        data = response.json()
+
+        return str(data)
+
+    except requests.exceptions.HTTPError as err:
+        print(f"HTTP error: {err}")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+    return None
--- llm/ai.py
+++ llm/ai.py
@@ -1,21 +1,36 @@
 from openai import OpenAI
 from settings import config
-from llm import kb
+from llm import kb, agent
 
 openai = OpenAI(
     base_url=config.LLM_BASE_URL,
     api_key=config.LLM_API_KEY
 )
-system_message = "You are a helpful assistant. Your name is 'Alex'"
+
+system_message = "You are a helpful assistant. Your name is 'Alex'."
+system_message += "Give short, courteous answers, no more than 1 sentence. "
+system_message += "Convert time to 12 hours clock if user is asking for 12hours clock. Your current time format is '%Y-%m-%d %H:%M:%S', when user ask for time then tell time only and for date tell date."
+system_message += "Always be accurate. If you don't know the answer, say so."
 
 def chat(text: str) -> str:
     history= kb.getHistory()
     kb.forUser(text)
     messages = [{"role": "system", "content": system_message}] + history + [{"role": "user", "content": text}]
     response = openai.chat.completions.create(
-        model=config.LLM_MODEL_NAME,
-        messages=messages,
+    	model=config.LLM_MODEL_NAME,
+     	messages=messages,
+      	tools=agent.getTools(),
     )
+
+    if response.choices[0].finish_reason=="tool_calls":
+        message = response.choices[0].message
+        response = agent.handle_tool_call(message)
+        messages.append(message)
+        messages.append(response)
+        response = openai.chat.completions.create(
+        	model=config.LLM_MODEL_NAME,
+         	messages=messages,
+        )
 
     res = response.choices[0].message.content or ""
     if len(res)>0:
